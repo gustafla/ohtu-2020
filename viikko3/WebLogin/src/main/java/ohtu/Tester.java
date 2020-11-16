91cb9ed790d8fc9ccebff605bbaf1ee187338197
Lauri Gustafsson <me@gustafla.space>
2020-11-16 22:28:38 +0200

    3.8 Add selenium browsing scenarios

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import java.util.Random;
 
 public class Tester {
 
     public static void main(String[] args) {
+        Random r = new Random();
         WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:4567");
@@ -28,6 +30,61 @@ public class Tester {
         element.submit();
 
         System.out.println(driver.getPageSource());
+
+        // Epäonnistunut kirjautuminen
+        driver.get("http://localhost:4567");
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkeo");
+        element = driver.findElement(By.name("login"));
+
+        element.submit();
+
+        System.out.println(driver.getPageSource());
+
+        // Käyttäjätunnuksen luominen
+        driver.get("http://localhost:4567");
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka" + r.nextInt(10000000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("asdf1234");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("asdf1234");
+        element = driver.findElement(By.name("signup"));
+
+        element.submit();
+
+        System.out.println(driver.getPageSource());
+
+        // ulkoskirjautuminen
+
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        System.out.println(driver.getPageSource());
+
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+
+        System.out.println(driver.getPageSource());
+
 
         driver.quit();
     }
