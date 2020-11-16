93a60874feda1bb9579d0b47c25effa912aa7684
Lauri Gustafsson <me@gustafla.space>
2020-11-16 23:13:00 +0200

    3.10 Add user creation scenarios
    YES I know git rm salasanat.txt won't remove it completely from this
    repository, but it's test junk and I don't wanna rebase now

--- viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -23,9 +23,36 @@ public class Stepdefs {
         element.click();
     }
 
+    @Given("command new user is selected")
+    public void newUserIsSelected() {
+        driver.get(baseUrl);
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+    }
+
     @When("correct username {string} and password {string} are given")
     public void correctUsernameAndPasswordAreGiven(String username, String password) {
         logInWith(username, password);
+    }
+
+    @When("a valid username {string} and password {string} and matching password confirmation are entered")
+    public void validUsernameAndPasswordAndConfirmationAreGiven(String username, String password) {
+        registerWith(username, password, password);
+    }
+
+    @When("an invalid username {string} and password {string} and matching password confirmation are entered")
+    public void invalidUsernameAndPasswordAndConfirmationAreGiven(String username, String password) {
+        registerWith(username, password, password);
+    }
+
+    @When("a valid username {string} and invalid password {string} and matching password confirmation are entered")
+    public void validUsernameAndInvalidPasswordAndConfirmationAreGiven(String username, String password) {
+        registerWith(username, password, password);
+    }
+
+    @When("a valid username {string} and valid password {string} and invalid password confirmation {string} are entered")
+    public void validUsernameAndPasswordAndInvalidConfirmationAreGiven(String username, String password, String passwordConfirmation) {
+        registerWith(username, password, passwordConfirmation);
     }
 
     @Then("user is logged in")
@@ -49,6 +76,16 @@ public class Stepdefs {
         pageHasContent("Give your credentials to login");
     }
 
+    @Then("user is not created and error {string} is reported")
+    public void userIsNotCreatedAndErrorIsReported(String error) {
+        pageHasContent(error);
+    }
+
+    @Then("a new user is created")
+    public void newUserIsCreated() {
+        pageHasContent("Welcome to Ohtu Application!");
+    }
+
     @After
     public void tearDown(){
         driver.quit();
@@ -67,6 +104,18 @@ public class Stepdefs {
         element = driver.findElement(By.name("password"));
         element.sendKeys(password);
         element = driver.findElement(By.name("login"));
+        element.submit();
+    }
+
+    private void registerWith(String username, String password, String passwordConfirmation) {
+        assertTrue(driver.getPageSource().contains("Create username and give password"));
+        WebElement element = driver.findElement(By.name("username"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("password"));
+        element.sendKeys(password);
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys(passwordConfirmation);
+        element = driver.findElement(By.name("signup"));
         element.submit();
     }
 }
