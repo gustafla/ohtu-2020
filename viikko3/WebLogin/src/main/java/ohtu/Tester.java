7708b3bf4d557cdd067953b67d45ce5e82294116
Lauri Gustafsson <me@gustafla.space>
2020-11-16 22:12:51 +0200

    3.8. Change Tester to use HtmlUnit driver

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -3,36 +3,35 @@ package ohtu;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:4567");
-        
-        sleep(2);
-        
+
+        System.out.println(driver.getPageSource());
+
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
-        sleep(2);
+        System.out.println(driver.getPageSource());
 
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
-        sleep(2);
+
         element.submit();
 
-        sleep(3);
-        
+        System.out.println(driver.getPageSource());
+
         driver.quit();
     }
-    
+
     private static void sleep(int n){
         try{
             Thread.sleep(n*1000);
