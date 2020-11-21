4b8350ab001cb46fd5b84370e0b5042ba07dea9c
Lauri Gustafsson <me@gustafla.space>
2020-11-21 16:39:12 +0200

    4.3 add @Before method to KauppaTest

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -7,27 +7,31 @@ import static org.mockito.Mockito.*;
 
 public class KauppaTest {
 
+    Pankki pankki;
+    Viitegeneraattori viitegeneraattori;
+    Varasto varasto;
+    Kauppa kauppa;
+
+    @Before
+    public void setup() {
+        varasto = mock(Varasto.class);
+        pankki = mock(Pankki.class);
+        viitegeneraattori = mock(Viitegeneraattori.class);
+        kauppa = new Kauppa(varasto, pankki, viitegeneraattori);
+    }
+
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
+        when(viitegeneraattori.uusi()).thenReturn(42);
 
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
-        // määritellään että viitegeneraattori palauttaa viitten 42
-        when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
         // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
-        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
 
-        // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);
-
         // tehdään ostokset
-        k.aloitaAsiointi();
-        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
-        k.tilimaksu("pekka", "12345");
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        kauppa.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
