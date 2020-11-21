0492d9fa601065e8da122e08367b4b2e21c1fe1c
Lauri Gustafsson <me@gustafla.space>
2020-11-21 17:03:32 +0200

    4.3 add more KauppaTest tests

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -15,19 +15,26 @@ public class KauppaTest {
     @Before
     public void setup() {
         varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        // määritellään että tuote numero 2 on talouspaperi jonka hinta on 15 ja saldo 1
+        when(varasto.saldo(2)).thenReturn(1);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "talouspaperi", 15));
+        // määritellään että tuote numero 3 on vessapaperi jonka hinta on 999 ja saldo 0
+        when(varasto.saldo(3)).thenReturn(0);
+        when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "vessapaperi", 999));
+
         pankki = mock(Pankki.class);
+
         viitegeneraattori = mock(Viitegeneraattori.class);
+        when(viitegeneraattori.uusi()).thenReturn(42);
+
         kauppa = new Kauppa(varasto, pankki, viitegeneraattori);
     }
 
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-        when(viitegeneraattori.uusi()).thenReturn(42);
-
-        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
-        when(varasto.saldo(1)).thenReturn(10);
-        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
         // tehdään ostokset
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
@@ -36,5 +43,46 @@ public class KauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
+    @Test
+    public void ostoksetMaksetaanTilisiirrolla() {
+        // tehdään ostokset
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        kauppa.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikein
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
+    }
+
+    @Test
+    public void kahdenEriTuotteenOstoksetTilisiirrolla() {
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(2);
+        kauppa.tilimaksu("leevi", "999");
+
+        verify(pankki).tilisiirto(eq("leevi"), eq(42), eq("999"), anyString(), eq(20));
+    }
+
+    @Test
+    public void kahdenSamanTuotteenOstoksetTilisiirrolla() {
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(1);
+        kauppa.tilimaksu("null", "6543");
+
+        verify(pankki).tilisiirto(eq("null"), eq(42), eq("6543"), anyString(), eq(10));
+    }
+
+    @Test
+    public void loppuneenTuotteenOstoksetTilisiirrolla() {
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(2);
+        kauppa.lisaaKoriin(3);
+        kauppa.tilimaksu("veeti", "23456");
+
+        verify(pankki).tilisiirto(eq("veeti"), eq(42), eq("23456"), anyString(), eq(15));
     }
 }
