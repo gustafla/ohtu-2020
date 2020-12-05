aef9cb7e0a647bad812ab80001f1dd0291265a68
Lauri Gustafsson <me@gustafla.space>
2020-12-05 11:23:46 +0200

    6.4 Add QueryBuilder and support

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -10,7 +10,10 @@ public class Main {
 
         QueryBuilder query = new QueryBuilder();
 
-        Matcher m = query.build();
+        Matcher m = query.playsIn("NYR")
+            .hasAtLeast(5, "goals")
+            .hasFewerThan(10, "goals")
+            .build();
 
         for (Player player : stats.matches(m)) {
             System.out.println(player);
--- viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
+++ viikko6/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -1,13 +1,27 @@
 package statistics;
 
-import statistics.matcher.Matcher;
-import statistics.matcher.All;
+import statistics.matcher.*;
 
 public class QueryBuilder {
     Matcher matcher;
 
     public QueryBuilder() {
         matcher = new All();
+    }
+
+    public QueryBuilder playsIn(String team) {
+        matcher = new And(matcher, new PlaysIn(team));
+        return this;
+    }
+
+    public QueryBuilder hasAtLeast(int value, String category) {
+        matcher = new And(matcher, new HasAtLeast(value, category));
+        return this;
+    }
+
+    public QueryBuilder hasFewerThan(int value, String category) {
+        matcher = new And(matcher, new HasFewerThan(value, category));
+        return this;
     }
 
     public Matcher build() {
