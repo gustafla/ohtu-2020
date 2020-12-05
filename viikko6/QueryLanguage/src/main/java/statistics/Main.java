de01d4acd71008706b2f58d31f6131de5ff5eabb
Lauri Gustafsson <me@gustafla.space>
2020-12-05 10:35:52 +0200

    6.3 Add matcher.Or

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -9,8 +9,12 @@ public class Main {
         Statistics stats = new Statistics(new PlayerReaderImpl(url));
 
         Matcher m = new And(
-            new HasFewerThan(1, "goals"),
-            new PlaysIn("NYR")
+            new HasAtLeast(50, "points"),
+            new Or(
+                    new PlaysIn("NYR"),
+                    new PlaysIn("NYI"),
+                    new PlaysIn("BOS")
+            )
         );
 
         for (Player player : stats.matches(m)) {
